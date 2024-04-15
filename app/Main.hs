2889cf7412a93e4ac387082fbb7e0e20ea6cda95
Ahmed Mogahed <ahmed.mogahed@me.com>
2024-04-15 20:56:09 +0200

    init

---
+++ app/Main.hs
@@ -0,0 +1,7 @@
+module Main (main) where
+
+import Problem1
+
+main :: IO ()
+main = do 
+    print problem1Simple
---
+++ src/Problem1.hs
@@ -0,0 +1,10 @@
+module Problem1 (problem1, problem1Simple) where
+
+problem1 :: Int
+problem1 = foldl (\acc n -> if divisibleBy3or5 n then acc + n else acc) 0 [1..1000-1]
+
+problem1Simple :: Int 
+problem1Simple = sum [n | n <- [1..1000-1], divisibleBy3or5 n]
+
+divisibleBy3or5 :: Int -> Bool
+divisibleBy3or5 n = mod n 3 == 0 || mod n 5 == 0
