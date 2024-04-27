a314a79e42e60b04dc920f8d09563f8b0fa5d120
Ahmed Mogahed <ahmed.mogahed@me.com>
2024-04-27 18:04:13 +0300

    Adding benchmarking support to project

--- app/Main.hs
+++ app/Main.hs
@@ -1,7 +1,18 @@
 module Main (main) where
 
 import Problem.Problem10.P10
+import Criterion.Main
+import Algorithm.Prime.PrimesGeneration
+
+-- main :: IO ()
+-- main = do
+--      print $ problem10
 
 main :: IO ()
-main = do
-    print $ problem10
+main = defaultMain 
+    [ bgroup "Problem 10" 
+        [ bench "1" $ whnf sum $ primes 10,
+          bench "5"  $ whnf sum $ primes 2000000
+        ]
+    ]
+    
